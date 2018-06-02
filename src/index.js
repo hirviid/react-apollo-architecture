9083dbe35428feccf5025f387cf308267b7af919
david@unbrace.be <david@unbrace.be>
2018-06-02 15:29:33 +0200

    :fire: Remove unused code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-
 import { ApolloProvider } from 'react-apollo';
 import client from './common/api/apolloConfig';
 import app from './packages/app';
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
